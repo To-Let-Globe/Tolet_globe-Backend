cd9f40c539e2efd46d6c24680ddb7605247ee222
Mikhiel <mikhielbenji9@gmail.com>
2024-06-25 18:25:52 +0530

    changes in the contact router

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const contactController = require("../controller/contact");
 
 // Route to handle contact form submission
-router.post("/contact", contactController.submitForm);
-router.get("/contact", contactController.getMessage);
+router.post("/", contactController.submitForm);
+router.get("/", contactController.getMessage);
 
 module.exports = router;
