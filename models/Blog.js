59a0f6389ad29e21a360c724fa94133ed33c10dd
Pratik <pratikyadav3949@gmail.com>
2024-07-15 11:53:57 +0530

    assest: add models and structure

--- models/Blog.js
+++ models/Blog.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    img: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+// const blogSchema = new mongoose.Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//     author: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     img: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
 
-const Blog = mongoose.model("Blog", blogSchema);
+// const Blog = mongoose.model("Blog", blogSchema);
 
-module.exports = Blog;
+// module.exports = Blog;
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
-// const router =require("express").Router();
-// const passport=require("passport");
+const router = require("express").Router();
+const passport = require("passport");
 
 // router.get("login/success",(req,res) => {
 //     if(req.user){
@@ -21,18 +21,18 @@
 // });
 
 router.get(
-    "/google/callback",
-    passport.authenticate("google",{
-        successRedirect:process.env.CLIENT_URL,
-        failureRedirect:"/login/failed",
-    })
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
 );
 
-router.get("/google",passport.authenticate("google",["profile","email"]));
+router.get("/google", passport.authenticate("google", ["profile", "email"]));
 
-router.get("/logout",(req,res) =>{
-    req.logout();
-    res.redirect(process.env.CLIENT_URL);
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
+});
 
 module.exports = router;
